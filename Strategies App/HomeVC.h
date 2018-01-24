f10563c692d017554793971c5bf237e8217f220e
Yerassyl Yerlanov <Yerassyl@MacBook-Pro-Yerassyl.local>
2018-01-24 15:32:30 +0600

    Initial commit

---
+++ Strategies App/HomeVC.h
@@ -0,0 +1,13 @@
+//
+//  HomeVC.h
+//  Strategies App
+//
+//  Created by Yerassyl Yerlanov on 24.01.2018.
+//  Copyright © 2018 AceLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeVC : UIViewController
+
+@end
---
+++ Strategies App/HomeVC.m
@@ -0,0 +1,28 @@
+//
+//  HomeVC.m
+//  Strategies App
+//
+//  Created by Yerassyl Yerlanov on 24.01.2018.
+//  Copyright © 2018 AceLight. All rights reserved.
+//
+
+#import "HomeVC.h"
+
+@interface HomeVC ()
+
+@end
+
+@implementation HomeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
